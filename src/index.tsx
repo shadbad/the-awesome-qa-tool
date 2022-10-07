c8ffb42cec75267438ebb0c7dd51dabe553a0c67
Sina Shadbad <shadbad@gmail.com>
2022-10-07 18:48:01 +0100

    Add basic project structure.

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+
+        <h1>The Awesome QA Tool</h1>
+
+    </React.StrictMode>
+);
+
+// eslint-disable-next-line no-console
+reportWebVitals(console.log);
